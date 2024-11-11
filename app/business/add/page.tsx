5cfff27350bd1095badb18f6188775da3fdddf78
Raanan Adut <radesigner84@gmail.com>
2024-11-11 22:05:26 +0200

    chore: Add columns for preview and form in business page

--- app/business/add/page.tsx
+++ app/business/add/page.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function AddBusinessPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      Add Business
+    <div className="flex flex-col lg:flex-row h-screen">
+      <div className="flex flex-col lg:w-1/2 p-4 lg:order-last lg:flex lg:justify-center lg:items-center">
+        Preview
+      </div>
+      <div className="flex flex-col lg:w-1/2 p-4 lg:order-first lg:flex lg:justify-center lg:items-center">
+        Form
+      </div>
     </div>
   );
 }
